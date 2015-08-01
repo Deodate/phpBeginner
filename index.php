354e957adc3510d51120a3518d2f1ac9fc98428d
beastar457 <beastar457@gmail.com>
2015-08-01 11:26:55 +0300

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- this is a comment a coment is a line of code that remind you what a code is doing and is nothing than to remind you -->
+<!-- the above comment work in html not in php below i am going to show you how to add comment to php -->
+<?php
+	// echo "string"; this is php comment because it is inside php tags 
+	/*  this another type of php comment  */
+	/* this index file is about to show you how to use this document:
+		every lesson have it's own purpose and is numbered from lesson1 up to lesson25
+		
+
+	  */
+?>
